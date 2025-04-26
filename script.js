fc3fbfcef38d31ed50b1e1f99795392e4a975f45
Artem Topolsky <almost210690@gmail.com>
2025-04-26 17:57:17 +0300

    return

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'Хватит', 'Бухать!','с Вовчиком!' ],
+			strings: [ 'HAPPY', 'BIRTHDAY','MY LOVE!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
-			
 			cx: w / 2,
 			cy: h / 2,
 			
